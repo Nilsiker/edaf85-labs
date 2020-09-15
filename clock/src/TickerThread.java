f1e9f354b5f6c5606ec9598c85b834136df14874
Nilsiker <nilsiker94@gmail.com>
2020-09-15 13:25:53 +0200

    fixed slight clock drift before tick()

--- clock/src/TickerThread.java
+++ clock/src/TickerThread.java
@@ -9,9 +9,13 @@ public class TickerThread extends Thread {
 
 	@Override
 	public void run() {
+		long t, diff;
+		t = System.currentTimeMillis();
 		while (true) {
 			try {
-				sleep(1000);
+				t += 1000;
+				diff = t - System.currentTimeMillis();
+				if(diff > 0) sleep(diff);
 				status.tick();	
 			} catch (InterruptedException e) {
 				
